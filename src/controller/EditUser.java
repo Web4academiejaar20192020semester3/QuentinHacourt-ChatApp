4a55ec08643257c354ce2ab947e8885f1b83fe63
QuentinHacourt <quentin.hacourt@student.ucll.be>
2020-08-15 00:10:36 +0200

    edit user

---
+++ src/controller/EditUser.java
@@ -0,0 +1,44 @@
+package controller;
+
+import domain.PersonService;
+import domain.Person;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+
+public class EditUser extends RequestHandler {
+    PersonService personService;
+
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String email;
+        String password;
+        String passwordRepeat;
+        String firstName;
+        String lastName;
+        String gender;
+        int age;
+        Person person;
+        personService = this.getPersonService();
+
+        email = (String) request.getParameter("email");
+        password = (String) request.getParameter("password");
+        passwordRepeat = (String) request.getParameter("passwordRepeat");
+        firstName = (String) request.getParameter("firstName");
+        lastName = (String) request.getParameter("email");
+        gender = (String) request.getParameter("gender");
+        age = Integer.parseInt((String) request.getParameter("age"));
+
+        if (password.equals(passwordRepeat)){
+            person = new Person(email, password, firstName, lastName, gender, age);
+            personService.updatePersons(person);
+        }
+
+		request.getRequestDispatcher("index.jsp").forward(request,response);
+    }
+}
